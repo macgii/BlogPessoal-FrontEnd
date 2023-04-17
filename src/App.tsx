1e58a9be9dabb0818aa2f5a9514c5bfe5f021dff
Giovana <maiiaraoliveiras1@gmail.com>
2023-04-17 00:36:04 -0300

    Criando componentes de login e refatorando estilos dos componetes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import React from "react";
-import { Grid } from "@material-ui/core";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./paginas/home/Home";
 import Navbar from "./components/statico/navbar/Navbar";
 import Footer from "./components/statico/footer/Footer";
+import Login from "./paginas/login/Login";
 import "./App.css";
+
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <Home />
+      <div>
+        <Routes>
+          {" "}
+          <Route path="/" element={<Login />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/components/statico/footer/Footer.tsx
+++ src/components/statico/footer/Footer.tsx
@@ -1,40 +1,68 @@
-import React from 'react';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import {Typography, Grid } from '@material-ui/core';
-import {Box} from '@mui/material';
+import React from "react";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import { Typography, Grid } from "@material-ui/core";
+import { Box } from "@mui/material";
+import "./Footer.css";
 
 function Footer() {
-    return (
-        <>
-            <Grid container direction="row" justifyContent="center" alignItems="center">
-                <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#3F51B5", height: "120px" }}>
-                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
-                        </Box>
-                        <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://github.com/macgii" target="_blank">
-                                <GitHubIcon style={{ fontSize: 60, color: "white" }} />
-                            </a>
-                            <a href="https://www.instagram.com/macgi__/" target="_blank">
-                                <InstagramIcon style={{ fontSize: 60, color: "white" }} />
-                            </a>
-                            <a href="https://www.linkedin.com/in/giovana-oliveira-dev/" target="_blank">
-                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
-                            </a>
-                        </Box>
-                    </Box>
-                    <Box style={{ backgroundColor: "#303F9F", height: "60px" }}>
-                        <Box paddingTop={2.5} >
-                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2020 Copyright:</Typography>
-                        </Box>
-                    </Box>
-                </Grid>
-            </Grid>
-        </>
-    )
+  return (
+    <>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid alignItems="center" item xs={12}>
+          <Box className="box1">
+            <Box
+              paddingTop={1}
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Typography
+                variant="h5"
+                align="center"
+                gutterBottom
+                className="textos"
+              >
+                Siga-nos nas redes sociais{" "}
+              </Typography>
+            </Box>
+            <Box display="flex" alignItems="center" justifyContent="center">
+              <a href="https://github.com/macgii" target="_blank">
+                <GitHubIcon className="redes" />
+              </a>
+              <a href="https://www.instagram.com/macgi__/" target="_blank">
+                <InstagramIcon className="redes" />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/giovana-oliveira-dev/"
+                target="_blank"
+              >
+                <LinkedInIcon className="redes" />
+              </a>
+            </Box>
+          </Box>
+          <Box className="box2">
+            <Box paddingTop={2.5}>
+              <Typography
+                variant="subtitle2"
+                align="center"
+                gutterBottom
+                className="textos"
+              >
+                © 2020 Copyright:
+              </Typography>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
 export default Footer;
