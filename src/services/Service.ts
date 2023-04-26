16a5cd75468057a91852a4272b116164cdb894c7
Giovana <maiiaraoliveiras1@gmail.com>
2023-04-26 01:05:56 -0300

    Criado models de Postagem e Tema e atualizando Service

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -13,3 +13,8 @@ export const login = async (url: string, dados: object, setDado: any) => {
   const resposta = await api.post(url, dados);
   setDado(resposta.data.token);
 };
+
+export const busca = async (url: string, setDado: any, header: any) => {
+  const resposta = await api.get(url, header);
+  setDado(resposta.data);
+};
