a4421fe6dbd091b90eeb86c1acbef5d05f5cbe15
Giovana <maiiaraoliveiras1@gmail.com>
2023-04-30 23:23:48 -0300

    Implementando CRUD de Tema

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -4,10 +4,10 @@ export const api = axios.create({
   baseURL: "https://blogpessoal-xd3x.onrender.com",
 });
 
-export const cadastroUsuario = async (url: string, dados: object, setDado: any) => {
-    const resposta = await api.post(url, dados);
-    setDado(resposta.data);
-  };
+export const cadastroUsuario = async (url: string, dados: object,  setDado: any) => {
+  const resposta = await api.post(url, dados);
+  setDado(resposta.data);
+};
 
 export const login = async (url: string, dados: object, setDado: any) => {
   const resposta = await api.post(url, dados);
@@ -17,4 +17,22 @@ export const login = async (url: string, dados: object, setDado: any) => {
 export const busca = async (url: string, setDado: any, header: any) => {
   const resposta = await api.get(url, header);
   setDado(resposta.data);
+};
+
+export const buscaId = async (url: string, setDado: any, header: any) => {
+  const resposta = await api.get(url, header);
+  setDado(resposta.data);
+};
+
+export const post = async(url: string, dados: any, setDado: any, header: any) => { 
+  const resposta = await api.post(url,dados,header)
+  setDado(resposta.data)
+}
+export const put = async (url: string,  dados: any,  setDado: any,  header: any) => {
+  const resposta = await api.put(url, dados, header);
+  setDado(resposta.data);
+};
+
+export const deleteId = async (url: string, header: any) => {
+  await api.delete(url, header);
 };
